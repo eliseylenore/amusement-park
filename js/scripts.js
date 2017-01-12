c1f4445cf96a10c9fe77b7a986d1146520ab8641
Elise St Hilaire and Kory Skarbek <kory@>
2017-01-12 11:11:32 -0800

    add javascript functions for showing and hiding ride when user selects height.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-  var canIRide = $("#feet").val();
-  $().submit(function(event){
+  $("form").submit(function(event){
+    $(".ride").hide();
+    var canIRide = $("#feet").val();
     event.preventDefault();
 
     if (canIRide === "under 4 feet") {
       $("#under4ft").show();
+      console.log(canIRide);
+
+    } else if (canIRide === "between 4-5 feet") {
+      $("#between4-5ft").show();
+      console.log(canIRide);
+    } else {
+      $("#over5ft").show();
     }
   });
 
